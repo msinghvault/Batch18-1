ed7d221c4f570da18d6ea0fdec532297e2fc6353
Jayram <jayram.rout@gmail.com>
2015-04-30 21:25:49 -0400

    Update

---
+++ CoreJava/src/Calculator.java
@@ -0,0 +1,11 @@
+
+public class Calculator {
+
+	/**
+	 * Anything with a name and  open brace and closing is a function or method.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hey how u doing.... ");
+	}
+}
