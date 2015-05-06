8a9328bd665e42f5ec5f7df65d5bba2fdf93beaa
Jayram <jayram.rout@gmail.com>
2015-05-05 20:37:38 -0400

    Update

---
+++ CoreJava/src/HelloWorld.java
@@ -0,0 +1,13 @@
+
+public class HelloWorld {
+	
+	/**
+	 * 
+	 * @param args
+	 */
+	
+	public static void main(String...args) {
+			System.out.println("Hello World".length());
+			System.out.println("Hello World".toUpperCase());
+	 }
+}
