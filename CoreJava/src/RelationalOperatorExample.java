4002c00c74639824f36cbc8d98633cc30aa037a4
Jayram <jayram.rout@gmail.com>
2015-05-09 21:44:08 -0400

    Update

---
+++ CoreJava/src/RelationalOperatorExample.java
@@ -0,0 +1,17 @@
+
+public class RelationalOperatorExample {
+
+	public static void main(String[] args) {
+		int arg1 = Integer.parseInt(args[0]);
+		int arg2 = Integer.parseInt(args[1]);
+		
+		int age = Integer.parseInt("jay");
+		System.out.println(age);
+		
+		String height = 34+"";
+		
+		// true or false
+		boolean isSame = (arg1 >= arg2);
+		System.out.println(isSame);
+	}
+}
